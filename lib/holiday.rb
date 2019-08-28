cb58e8d411dcd460b1338888ec3ec374f974d48e
Baktiar Miah <baktiar.m@gmail.com>
2019-08-28 14:01:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -90,6 +90,21 @@ holiday_hash.each do |season, holiday|
   end 
 end
 
+def all_holidays_with_bbq(holiday_hash)
+  # return an array of holiday names (as symbols) where supply lists
+  # include the string "BBQ"
+  bbq_holidays = []
+  
+  holiday_hash.each do |season, holiday|
+    holiday.each do |holiday, supply|
+      if supply.include?("BBQ")
+        bbq_holidays << holiday
+      end
+    end
+  end
+
+  bbq_holidays
+end
 
 
 
